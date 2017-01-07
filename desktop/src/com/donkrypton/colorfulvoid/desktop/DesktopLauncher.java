0e31edef726339094c13e862f4b87604785f3433
bob <bob@example.com>
2017-01-07 19:13:36 +0100

    Initial commit

---
+++ desktop/src/com/donkrypton/colorfulvoid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.donkrypton.colorfulvoid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.donkrypton.colorfulvoid.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
